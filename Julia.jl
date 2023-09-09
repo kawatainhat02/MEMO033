1ea1b8e17e87b1bcfba20a92f49744246206663a
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-09 17:02:54 +0900

    Create Julia.jl

---
+++ Julia.jl
@@ -0,0 +1,20 @@
+println("Hello, World")
+A = randn(10, 10)
+B = randn(10, 10)
+C = A*B
+# Assign the value 10 to the variable x
+julia> x = 10
+10
+
+# Doing math with x's value
+julia> x + 1
+11
+
+# Reassign x's value
+julia> x = 1 + 1
+2
+
+# You can assign values of other types, like strings of text
+julia> x = "Hello World!"
+"Hello World!"
+
